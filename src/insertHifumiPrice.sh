8a4c81fc3316bb921a3ed40908148e629a59dc85
Luo Hongshuai <lhsfboy@gmail.com>
2015-12-08 13:56:10 +0900

    20151208_Daily_Comment

--- src/insertHifumiPrice.sh
+++ src/insertHifumiPrice.sh
@@ -1,2 +1,3 @@
-python2.7 ../../PythonWatchHifumiPrice/main.py >> ../index.html
+cd /root/git/lhsfcboy.github.io/src/
+/usr/local/bin/python2.7 ../../PythonWatchHifumiPrice/main.py >> ../index.html
 echo  "<br/>" >> ../index.html
