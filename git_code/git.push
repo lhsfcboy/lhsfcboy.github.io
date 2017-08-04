977b8d039b17b028ff837fa8e36bb23ef3d51290
Luo Hongshuai <lhsfboy@gmail.com>
2017-08-04 17:29:01 +0900

    Clean up the structure

---
+++ git_code/git.push
@@ -0,0 +1,1 @@
+git add -A; git commit -m "`date +"%F%t%T"` Daily Commit";git push;
--- src/insertHifumiPrice.sh
+++
@@ -1,3 +0,0 @@
-cd /root/git/lhsfcboy.github.io/src/
-/usr/local/bin/python2.7 ../../PythonWatchHifumiPrice/main.py >> ../index.html
-echo  "<br/>" >> ../index.html
