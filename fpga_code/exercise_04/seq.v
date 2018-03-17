2cd173e77b57ab022afef7c85595d8966abb5ae5
Mike Hongshuai Luo <lhsfcboy@gmail.com>
2018-03-17 15:30:05 +0900

    Update seq.v

--- fpga_code/exercise_04/seq.v
+++ fpga_code/exercise_04/seq.v
@@ -1,23 +1,25 @@
-module ex4(input wire clk, rst, B,
+module Volume(input wire clk, rst, B,
           output reg S1,S0);
+          
     reg S1_dash, S0_dash;
 
-    assign S1_dash = ~B &  S1 & ~S0 |
-                     ~B &  S1 &  S0 |
-                      B & ~S1 &  S0 |
-                      B &  S1 & ~S0 ; 
-    assign S0_dash = ~B & ~S1 &  S0 |
-                     ~B &  S1 &  S0 |
-                      B & ~S1 & ~S0 |
-                      B &  S1 & ~S0 ;
-
-    always @(poesedge clk)
+    always @(B,S1,S0)
         begin
-            if(!rst)
-                S1 <= 0;
-                S0 <= 0;
+            S1_dash = (~B &  S1 & ~S0) |  (~B &  S1 &  S0) | (B & ~S1 &  S0) | (B &  S1 & ~S0); 
+            S0_dash = (~B & ~S1 &  S0) | (~B &  S1 &  S0) | (B & ~S1 & ~S0) |(B &  S1 & ~S0) ;
+        end
+
+    always @(posedge clk)
+        begin
+            if(rst)
+                begin
+                    S1 <= 0;
+                    S0 <= 0;
+                end
             else
-                S1 <= S1_dash;
-                S0 <= S0_dash;
+                begin
+                    S1 <= S1_dash;
+                    S0 <= S0_dash;
+                end
         end
 endmodule
