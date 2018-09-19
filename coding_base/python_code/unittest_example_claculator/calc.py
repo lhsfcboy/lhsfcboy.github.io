4371aab331a4a4d57000a4303a4085a4ca94ecff
Luo Mike Hongshuai <lhsfcboy@gmail.com>
2018-09-19 17:41:50 +0900

    2018-09-19	17:41:50 Daily Commit

---
+++ coding_base/python_code/unittest_example_claculator/calc.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+#  coding:utf8
+
+class Calculator(object):
+
+    # 加算
+    def add(self, num1, num2):
+        return num1 + num2
+
+    # 減算
+    def sub(self, num1, num2):
+        return num1 - num2
+
+    # 乗算
+    def mul(self, num1, num2):
+        return num1 * num2
+
+    # 除算
+    def div(self, num1, num2):
+        return num1 // num2
