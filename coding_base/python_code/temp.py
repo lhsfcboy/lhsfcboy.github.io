2ca955e5f94a932510d0d07f53978d400d691a0b
Mike Hongshuai Luo <lhsfcboy@gmail.com>
2018-12-20 09:13:45 +0900

    Update temp.py

--- coding_base/python_code/temp.py
+++ coding_base/python_code/temp.py
@@ -1,14 +1,19 @@
+#!/usr/bin/env python
 
-from math import modf
+from time import sleep
+import random
+import logging
+logging.basicConfig(level=logging.INFO,format="%(asctime)s.%(msecs)03d[%(levelname)-8s]:%(created).6f %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 
+while True:
+    sleep(15-(int(time.time())+2) % 15)
+    logging.info(f"{int(time.time()) % 10}")
 
-
-
-
-for i in range(100):
-    print(price_to_quantity(1000000 + i))
-
-
-
-# for i in range(100):
-#     print((400899 + i)/100000000)
+print("hello~")
+logging.error("出现了错误")
+sleep(1)
+logging.info("bs_amount_ratio is 1.834679120207966")
+sleep(1)
+logging.warning("警告信息")
+sleep(1)
+logging.critical ("critical")
