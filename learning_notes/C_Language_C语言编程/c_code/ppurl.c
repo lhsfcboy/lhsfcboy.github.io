610f4cdb7b7c38ca23a0692cfde1ad1bd5daa4e8
Mike Hongshuai Luo <lhsfcboy@gmail.com>
2024-10-14 10:27:08 +0900

    Update ppurl.c

--- learning_notes/C_Language_C语言编程/c_code/ppurl.c
+++ learning_notes/C_Language_C语言编程/c_code/ppurl.c
@@ -2,7 +2,9 @@
 
 int main()
 {
-	    int i=43;
-	        printf("%d\n",printf("%d",printf("%d",i)));
-		    return 0;
+    int i = 43;
+    printf("%d\n", printf("%d", printf("%d", i)));
+    return 0;
 }
+
+// 理解 程序的执行 和 程序的返回值
