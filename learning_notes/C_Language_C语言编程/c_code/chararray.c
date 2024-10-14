17699fa8da7192f8592dde23c46a4382619544ea
Mike Hongshuai Luo <lhsfcboy@gmail.com>
2024-10-14 10:21:26 +0900

    Update chararray.c

--- learning_notes/C_Language_C语言编程/c_code/chararray.c
+++ learning_notes/C_Language_C语言编程/c_code/chararray.c
@@ -1,15 +1,13 @@
-int main(){
-	char chars1 [] = "hello12345hello";
+#include <stdio.h>
 
-	char chars2[4] = "12ii";
-	
-	char chars3[9]= "12345678";
+int main(){
+	char chars1 []  = "hello12345hello";
+	char chars2 [4] = "12ii";
+	char chars3 [9] = "12345678";
 
 	printf("%s\n",chars1);
 	printf("%s\n",chars2);
 	printf("%s\n",chars3);
-
-
-
+	// 注意观察chars2是如何`入侵`了chars3的领域
 	return 0;
 }
