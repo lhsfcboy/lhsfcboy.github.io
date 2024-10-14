64170ee788148b549c8d9c4fe79ac026215dfa6e
Mike Hongshuai Luo <lhsfcboy@gmail.com>
2024-10-14 10:31:55 +0900

    Update start.c

--- learning_notes/C_Language_C语言编程/c_code/start.c
+++ learning_notes/C_Language_C语言编程/c_code/start.c
@@ -1,30 +1,43 @@
+// 生成一个柱状图
+// 4       3       3       1       9       3       5       6       7       0
+// *       *       *               *       *       *       *       *       
+// *       *       *               *       *       *       *       *       
+// *       *       *               *       *       *       *       *       
+// *                               *               *       *       *       
+//                                 *               *       *       *       
+//                                 *                       *       *       
+//                                 *                       *       *       
+//                                 *                               *       
+//                                 *
+
 #include <stdio.h>
 #define N 10
+
 int main(void)
 {
-	int a [N] = {4,3,3,1,9,3,5,6,7,};
-	int i = 0, j = 0;
-	int max = 0;
-	for (i = 0;i < N;i++){
-		if (max < a[i]){
-			max = a[i];	
-		}	
+    int a[N] = {4, 3, 3, 1, 9, 3, 5, 6, 7, 0};
+    int i = 0, j = 0;
+    int max = 0;
 
-		printf ("%d\t",a[i]);
-	}
-	printf("\n");
-	
-	for (i = 0;i < max;i++){
-		for (j = 0; j < N; j++){
-			if(i < a[j]){
-				printf("*");
-			}
-			printf("\t");
-		}
-	
-		printf("\n");
-	
-	}
-			
-	return 0;
+    // Find max value and print array
+    for (i = 0; i < N; i++) {
+        if (max < a[i]) {
+            max = a[i];
+        }
+        printf("%d\t", a[i]);
+    }
+    printf("\n");
+
+    // Print histogram
+    for (i = 0; i < max; i++) {
+        for (j = 0; j < N; j++) {
+            if (i < a[j]) {
+                printf("*");
+            }
+            printf("\t");
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
